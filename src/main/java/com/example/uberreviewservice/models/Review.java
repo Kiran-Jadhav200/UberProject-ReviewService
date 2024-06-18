93152d02185189ecfba8a9062778231ae94c9f19
Kiran Jadhav <jadhavrkiran21@gmail.com>
2024-06-18 12:32:46 +0530

    annotations setup

--- src/main/java/com/example/uberreviewservice/models/Review.java
+++ src/main/java/com/example/uberreviewservice/models/Review.java
@@ -1,16 +1,35 @@
 package com.example.uberreviewservice.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
-@Entity
+import java.util.Date;
+
+@Entity()
+@Table(name = "bookingReview")
 
 public class Review {
     @Id //this annotation makes the id property a primary key of our table
+   @GeneratedValue(strategy = GenerationType.IDENTITY)// identity means auto_increment
     Long id;
+    @Column(nullable = false)
+    String content;
 
     @Column
-    String name;
+    Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)// this annotations tells spring about the formats of date object to be stored i.e. Date/ Time ? TimeStamp
+   @CreatedDate         // this annotations tells spring that only handle it for object creation
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+     @LastModifiedDate  //this annotations tells that only handles it for object update
+    Date updatedAt;
 
 }
+
+// new Review(content, rating)
